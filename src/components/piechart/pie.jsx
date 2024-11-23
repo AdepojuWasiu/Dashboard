de52b6d16a7ee63d9200cb220f525235347b6b94
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-11-23 10:25:08 +0100

    fix typo

--- src/components/piechart/pie.jsx
+++ src/components/piechart/pie.jsx
@@ -6,39 +6,13 @@ import { PieChart, Pie, Cell } from "recharts";
 const Piechart = () => {
 
   const data = [
-    { name: "Completed", value: 82.3 }, // Percentage to fill
-    { name: "Remaining", value: 17.7 } // Remaining percentage
+    { name: "Completed", value: 82.3 }, 
+    { name: "Remaining", value: 17.7 } 
   ];
 
-  const colors = ["#6C5DD3", "#e0e0e0"]; // Adjust colors for filled and background
+  const colors = ["#6C5DD3", "#e0e0e0"];
   return (
-    // <div>
-    //   <PieChart width={200} height={200}>
-    //   <Pie
-    //     data={data}
-    //     dataKey="value"
-    //     startAngle={90}
-    //     endAngle={-270}
-    //     innerRadius={70}
-    //     outerRadius={90}
-    //     paddingAngle={5}
-    //   >
-    //     {data.map((entry, index) => (
-    //       <Cell key={`cell-${index}`} fill={colors[index]} />
-    //     ))}
-    //   </Pie>
-    //   <text
-    //     x="50%"
-    //     y="50%"
-    //     textAnchor="middle"
-    //     dominantBaseline="middle"
-    //     style={{ fontSize: "18px", fontWeight: "bold" }}
-    //   >
-    //     82.3%
-    //   </text>
-    // </PieChart>
-    // </div>
-
+  
     <div>
     <PieChart width={58} height={58}>
     <Pie
@@ -59,7 +33,7 @@ const Piechart = () => {
       y="50%"
       textAnchor="middle"
       dominantBaseline="middle"
-      style={{ fontSize: "13px", fontWeight: "600", color: '#6C5DD3' }}
+      style={{ fontSize: "13px", fontWeight: "600", color: '#0049C6' }}
     >
       86%
     </text>
